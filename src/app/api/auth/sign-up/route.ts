6b756d3eb03470d05a8770885dc60784d9a9c132
Aranchai Moonkum <aranchai@Aranchais-MacBook-Pro.local>
2025-01-21 17:58:42 +0700

    initital commit for auth-system branch

---
+++ src/app/api/auth/sign-up/route.ts
@@ -0,0 +1,64 @@
+import { prisma } from "@/lib/prisma"
+import bcrypt from "bcrypt"
+
+export async function POST(req: Request) {
+    try {
+        const { username, email, password, phone } = await req.json()
+
+        // check for existing email
+        const existingEmail = await prisma.user.findUnique({
+            where: { email }
+        })
+        if (existingEmail) {
+            return Response.json(
+                { message: "Email already exists" },
+                { status: 400 }
+            )
+        }
+
+        // check for existing username
+        const existingUsername = await prisma.user.findUnique({
+            where: { username }
+        })
+        if (existingUsername) {
+            return Response.json(
+                { message: "Username already exists" },
+                { status: 400 }
+            )
+        }
+
+        // check for existing phone
+        const existingPhone = await prisma.user.findUnique({
+            where: { phone }
+        })
+        if (existingPhone) {
+            return Response.json(
+                { message: "Phone number already exists" },
+                { status: 400 }
+            )
+        }
+
+        // if no conflicts exist, create the new user
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+                phone,
+            },
+        })
+
+        return Response.json({
+            message: "User created successfully",
+            data: newUser
+        }, { status: 201 })
+
+    } catch (error: any) {
+        console.error("Server error:", error)
+        return Response.json({
+            message: "Something went wrong",
+            error: error.message
+        }, { status: 500 })
+    }
+}
