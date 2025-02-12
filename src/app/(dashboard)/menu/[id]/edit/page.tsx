ef339be0de0eb266bd749d2acf1a6ac6ffb7ca8e
Aranchai Moonkum <aranchai@Aranchais-MacBook-Pro.local>
2025-02-12 20:27:47 +0700

    added background for edit menu page

--- src/app/(dashboard)/menu/[id]/edit/page.tsx
+++ src/app/(dashboard)/menu/[id]/edit/page.tsx
@@ -15,7 +15,7 @@ const EditMenuPage = async ({ params }: PageProps) => {
     })
     return (
         <div className="w-full">
-            <div className="bg-white= p-5 rounded-xl">
+            <div className="bg-white p-5 rounded-xl">
                 <h1 className="text-2xl font-semibold flex justify-center">
                     Edit menu
                 </h1>
