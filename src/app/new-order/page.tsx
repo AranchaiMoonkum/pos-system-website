f4bc017283374a3fdcfa8ad1c0e94e75b1a75daa
Aranchai Moonkum <aranchai@Aranchais-MacBook-Pro.local>
2025-02-26 11:45:49 +0700

    cleaned new-order page

--- src/app/new-order/page.tsx
+++ src/app/new-order/page.tsx
@@ -1,23 +1,14 @@
-import React from "react"
-import MenuList from "./components/(section)/menulist"
-import CheckoutType from "./components/(checkout)/checkoutType"
-import CheckoutList from "./components/(checkout)/checkoutList"
+import { MenuList } from "@/app/new-order/components/(menu)/MenuList"
+import { CheckOut } from "@/app/new-order/components/(checkout)/CheckOut"
 
-export default function NewOrder() {
+const NewOrder = () => {
     return (
-        <div className="w-full flex gap-5">
-            <div className="w-8/12 bg-white p-5 rounded-xl">
-                <MenuList />
-            </div>
-        <div className="w-4/12 bg-white rounded-xl h-5/6">
-          <div className="p-5">
-            <h1 className="text-4xl text-jade">Checkout</h1>
-          <span>Order #31</span>
-          </div>
-          
-          <CheckoutType />
-          {/* <CheckoutList/> */}
-        </div>
-        </div>
+        <article className="w-full flex gap-5 mb-5">
+            <MenuList />
+        
+            <CheckOut />
+        </article>
     )
 }
+
+export default NewOrder
