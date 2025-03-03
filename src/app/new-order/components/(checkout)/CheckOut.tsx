3faf9d3cecd8f03233664b8aa7b2c93e09d6b978
Poraparint <poraparint@gmail.com>
2025-03-03 10:12:52 +0700

    fixed checkout to sticky

--- src/app/new-order/components/(checkout)/CheckOut.tsx
+++ src/app/new-order/components/(checkout)/CheckOut.tsx
@@ -75,102 +75,108 @@ export default function CheckOut() {
     }
 
     return (
-        <section className="w-4/12 bg-white rounded-xl h-5/6">
+        <section className="w-4/12 relative">
             <div className="relative h-full">
-                <h2 className="text-2xl font-bold p-4">Checkout</h2>
-                <ScrollArea className="max-h-[400px] overflow-y-auto z-10">
-                    <Table>
-                        <TableHeader className="bg-jade text-white ">
-                            <TableRow className="hover:bg-jade">
-                                <TableHead className="flex text-white">
-                                    Name
-                                </TableHead>
-                                <TableHead className="text-white">
-                                    QTY
-                                </TableHead>
-                                <TableHead className="text-white">
-                                    Total price
-                                </TableHead>
-                                <TableHead></TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {cart.length === 0 ? (
-                                <TableRow>
-                                    <TableCell>Your cart is empty.</TableCell>
+                <div className="bg-white h-5/6 rounded-xl sticky top-0">
+                    <h2 className="text-2xl font-bold p-4">Checkout</h2>
+                    <ScrollArea className="max-h-[400px] overflow-y-auto z-10">
+                        <Table>
+                            <TableHeader className="bg-jade text-white ">
+                                <TableRow className="hover:bg-jade">
+                                    <TableHead className="flex text-white">
+                                        Name
+                                    </TableHead>
+                                    <TableHead className="text-white">
+                                        QTY
+                                    </TableHead>
+                                    <TableHead className="text-white">
+                                        Total price
+                                    </TableHead>
+                                    <TableHead></TableHead>
                                 </TableRow>
-                            ) : (
-                                cart.map((item) => (
-                                    <TableRow
-                                        key={`${item.id}-${item.description}`}
-                                        className="border-b py-2"
-                                    >
-                                        <TableCell className="flex">
-                                            <div className="flex flex-col">
-                                                <p>{item.name}</p>
-                                                {item.description && (
-                                                    <p className="text-pebble">
-                                                        {item.description}
-                                                    </p>
-                                                )}
-                                            </div>
-                                        </TableCell>
-                                        <TableCell>{item.quantity}</TableCell>
-
+                            </TableHeader>
+                            <TableBody>
+                                {cart.length === 0 ? (
+                                    <TableRow>
                                         <TableCell>
-                                            <div className="flex justify-center">
-                                                <p>
-                                                    {(
-                                                        item.price *
-                                                        item.quantity
-                                                    ).toFixed(2)}{" "}
-                                                    ฿
-                                                </p>
-                                            </div>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Button
-                                                onClick={() =>
-                                                    clearCartItem(
-                                                        item.id,
-                                                        item.description
-                                                    )
-                                                }
-                                                variant="destructive"
-                                            >
-                                                <Trash />
-                                            </Button>
+                                            Your cart is empty.
                                         </TableCell>
                                     </TableRow>
-                                ))
-                            )}
-                        </TableBody>
-                    </Table>
-                    <ScrollBar orientation="vertical" />
-                </ScrollArea>
-                {cart.length > 0 && (
-                    <div className="p-2 absolute h-full w-full flex flex-col justify-between left-0 top-0">
-                        <Button
-                            onClick={clearCart}
-                            className="w-10 self-end "
-                            variant="destructive"
-                        >
-                            <Trash />
-                        </Button>
-                        <div>
-                            <div className="my-4 py-2 border-y flex justify-between font-medium">
-                                <p>Total Price :</p>{" "}
-                                <p>{totalPrice.toFixed(2)} ฿</p>
-                            </div>
+                                ) : (
+                                    cart.map((item) => (
+                                        <TableRow
+                                            key={`${item.id}-${item.description}`}
+                                            className="border-b py-2"
+                                        >
+                                            <TableCell className="flex">
+                                                <div className="flex flex-col">
+                                                    <p>{item.name}</p>
+                                                    {item.description && (
+                                                        <p className="text-pebble">
+                                                            {item.description}
+                                                        </p>
+                                                    )}
+                                                </div>
+                                            </TableCell>
+                                            <TableCell>
+                                                {item.quantity}
+                                            </TableCell>
+
+                                            <TableCell>
+                                                <div className="flex justify-center">
+                                                    <p>
+                                                        {(
+                                                            item.price *
+                                                            item.quantity
+                                                        ).toFixed(2)}{" "}
+                                                        ฿
+                                                    </p>
+                                                </div>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Button
+                                                    onClick={() =>
+                                                        clearCartItem(
+                                                            item.id,
+                                                            item.description
+                                                        )
+                                                    }
+                                                    variant="destructive"
+                                                >
+                                                    <Trash />
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
+                                )}
+                            </TableBody>
+                        </Table>
+                        <ScrollBar orientation="vertical" />
+                    </ScrollArea>
+                    {cart.length > 0 && (
+                        <div className="p-2 absolute h-full w-full flex flex-col justify-between left-0 top-0">
                             <Button
-                                onClick={handleCheckout}
-                                className="h-16 w-full"
+                                onClick={clearCart}
+                                className="w-10 self-end "
+                                variant="destructive"
                             >
-                                Checkout
+                                <Trash />
                             </Button>
+                            <div>
+                                <div className="my-4 py-2 border-y flex justify-between font-medium">
+                                    <p>Total Price :</p>{" "}
+                                    <p>{totalPrice.toFixed(2)} ฿</p>
+                                </div>
+                                <Button
+                                    onClick={handleCheckout}
+                                    className="h-16 w-full"
+                                >
+                                    Checkout
+                                </Button>
+                            </div>
                         </div>
-                    </div>
-                )}
+                    )}
+                </div>
             </div>
         </section>
     )
